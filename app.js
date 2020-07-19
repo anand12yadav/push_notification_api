e98252cab80be35d35d954f354263cc256cf1e47
mayur10 <mayurb609@gmail.com>
2020-07-19 21:00:44 +0530

    added a new route for v1 fcm API.

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+const {google}=require("googleapis");
+const MESSAGING_SCOPE="https://www.googleapis.com/auth/firebase.messaging";
+const SCOPES=[MESSAGING_SCOPE];
+
 const express = require("express");
 const app = express();
 
@@ -22,16 +26,23 @@ const data = {
     key2: "value2"
 };
 
-function sendNotification(message){
+function sendNotification(message, apiVersion=0, access_token=''){
     return new Promise((resolve, reject) => {
-        const Headers = {
-            "Content-Type": "application/json",
-            "Authorization": "key="+process.env.FCM_API_KEY
-        };
-    
-        // const URL = "https://fcm.googleapis.com/v1/projects/pushnotification-f3008/messages:send";
+        let Headers, URL;
 
-        const URL = "https://fcm.googleapis.com/fcm/send";
+        if(apiVersion === 1){
+            Headers = {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer "+ access_token
+            };
+            URL = "https://fcm.googleapis.com/v1/projects/pushnotification-f3008/messages:send";
+        }else{
+            Headers = {
+                "Content-Type": "application/json",
+                "Authorization": "key="+process.env.FCM_API_KEY
+            };
+            URL = "https://fcm.googleapis.com/fcm/send";
+        }
 
         request.post({
             headers: Headers,
@@ -41,6 +52,25 @@ function sendNotification(message){
             resolve(body);
         });
     });
+}
+
+function getAccessToken(){
+    return new Promise(function(resolve,reject){
+        const jwtClient=new google.auth.JWT(
+            process.env.FCM_CLIENT_EMAIL,
+            null,
+            process.env.FCM_PRIVATE_KEY,
+            SCOPES,
+            null
+        )
+        jwtClient.authorize(function(err,tokens){
+            if(err){
+                reject(err)
+                return
+            }
+            resolve(tokens.access_token)
+        })
+    })
 }
 
 router.post("/pushNotification", (req, res) => {
@@ -65,17 +95,60 @@ router.post("/pushNotification", (req, res) => {
             "data": data
         };   
     }
-    sendNotification(message).then(
+    sendNotification(message, 0, '').then(
         success => {
             return res.json({"Error": false, "Response": success});
         }
-    )
+    );
     
+});
+
+
+router.post("/pushNotification/v1", (req, res) => {
+    const token = req.body.token;
+    const type = req.body.type;
+    let message;
+
+    if(type === 'notification'){
+        message = {
+            "message": {
+                "token": token,
+                "notification": notification
+            }
+        };
+    } else if(type === 'data'){
+        message = {
+            "message": {
+                "token": token,
+                "data": data
+            }
+        };
+    } else{
+        message = {
+            "message": {
+                "token": token,
+                "notification": notification,
+                "data": data
+            }
+        };   
+    }
+    getAccessToken().then(
+        token => {
+            sendNotification(message, 1, token).then(
+                success => {
+                    return res.json({"Error": false, "Response": success});
+                }
+            );
+        }, err => {
+            console.log(err);
+            return res.json({"Error": false, "Response": "Something Went Wrong"});
+        }
+    );
 });
 
 
 app.use("/api", router);
 
 app.listen(port, () => {
-    console.log("listening to port" + port)
+    console.log("listening to port => " + port)
 });
