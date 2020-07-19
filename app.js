a3c23599ad6d5781c3f6410995d58750135e7cf8
mayur-bhandari <mayur.bhandari@inscripts.com>
2020-07-19 14:33:21 +0530

    Cleaning repo, using env variables, made function for sendNotification and changed /pushNotification route.

--- app.js
+++ app.js
@@ -1,87 +1,91 @@
-const {google}=require("googleapis")
-const MESSAGING_SCOPE="https://www.googleapis.com/auth/firebase.messaging"
-const SCOPES=[MESSAGING_SCOPE]
+const express = require("express");
+const app = express();
 
-const express=require("express")
-const app=express()
+const bodyParser = require("body-parser");
+const router = express.Router();
+const request = require("request");
 
-const bodyParser=require("body-parser")
-const router=express.Router()
-const request=require("request")
+const dotenv = require('dotenv');
+const port = 8080;
 
-const port=8080
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+dotenv.config();
 
-app.use(bodyParser.urlencoded({extended:true}))
-app.use(bodyParser.json())
+const notification = {
+    title: "A Push Notification Test",
+    body: "A Test Body"
+};
 
-router.post("/pushnotification",(req,res)=>{
+const data = {
+    key1: "value1",
+    key2: "value2"
+};
 
-    getAccessToken().then(function(access_token){
-        const title=req.body.title
-        const body=req.body.body
-        const token=req.body.token
-        const key1=req.body.key1
-        const key2=req.body.key2
-
+function sendNotification(message){
+    return new Promise((resolve, reject) => {
+        const Headers = {
+            "Content-Type": "application-json",
+            "Authorization": "key="+process.env.FCM_API_KEY
+        };
+    
+        const URL = "https://fcm.googleapis.com/v1/projects/pushnotification-f3008/messages:send";
 
         request.post({
-            headers:{
-                Authorization:"Bearer "+ access_token
-            },
-            url:"https://fcm.googleapis.com/v1/projects/pushnotification-f3008/messages:send",
-            body:JSON.stringify(
-                {
-                    
-                        "message":{
-                          "token":token,
-                          "notification":{
-                            "title":title,
-                            "body":body
-                          },
-                          "data":{
-                            "key1" : key1,
-                            "key2" : key2
-                            
-                          }
-                        }
-                      
-               }
-
-            )
-        },function(error,response,body){
-                res.send(body)
-                console.log(body)
-        })
-
-    })
- })
-
-
-app.use("/api",router)
-app.listen(port,()=>{
-    console.log("listening to port" + port)
-})
-
-function getAccessToken(){
-    return new Promise(function(resolve,reject){
-        const key=require("./service-account.json")
-        const jwtClient=new google.auth.JWT(
-            key.client_email,
-            null,
-            key.private_key,
-            SCOPES,
-            null
-        )
-        jwtClient.authorize(function(err,tokens){
-            if(err){
-                reject(err)
-                return
+            headers: Headers,
+            url: URL,
+            body: JSON.stringify(message)
+        }, function (error, response, body) {
+            if(error){
+                return reject(error);
+            }else{
+                return resolve(body);
             }
-            resolve(tokens.access_token)
-        })
-    })
+        });
+    });
 }
 
-getAccessToken().then(function(access_token){
-    console.log(access_token)
-})
+router.post("/pushNotification", (req, res) => {
+    const token = req.body.token;
+    const type = req.body.type;
+    const message;
+
+    if(type === 'notification'){
+        message = {
+            "message": {
+                "token": token,
+                "notification": notification
+            }
+        };
+    } else if(type === 'data'){
+        message = {
+            "message": {
+                "token": token,
+                "data": data
+            }
+        };
+    } else{
+        message = {
+            "message": {
+                "token": token,
+                "notification": notification,
+                "data": data
+            }
+        };   
+    }
+    sendNotification.then(
+        success => {
+            return res.json({"Error": false, "Response": JSON.stringify(success)});
+        }, error => {
+            return res.json({"Error": true, "Response": "Something Went Wrong"});
+        }
+    )
+    
+});
+
+
+app.use("/api", router);
+
+app.listen(port, () => {
+    console.log("listening to port" + port)
+});
